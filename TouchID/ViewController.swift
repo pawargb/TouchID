0d7702974a7498a5bbc309772196ba29a21b49f6
Ganesh <pawargb.2071990@gmail.com>
2019-09-12 12:54:46 +0530

    Changed LAPolicy

--- TouchID/ViewController.swift
+++ TouchID/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
-
+    @IBAction func touchButtonClicked(_ sender: Any) {
+        
+        let context: LAContext = LAContext()
+        if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: nil){
+            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: "Need your login to authenticate") { (wasSuccess, errorMessage) in
+                
+                if wasSuccess{
+                    print("was success")
+                }else{
+                    print("wasn't success, error : \(errorMessage?.localizedDescription)")
+                    
+                }
+            }
+        }
+    }
+    
 }
 
